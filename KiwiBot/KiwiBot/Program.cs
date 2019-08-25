2dba863d035496f80cf939de8828402611b3ccfe
metalcow-student <46615373+metalcow-student@users.noreply.github.com>
2019-08-24 23:22:13 -0500

    Directions Algorightm Update
    simplified movement and added rotation based on Thad's Wilson math code.

--- KiwiBot/KiwiBot/Program.cs
+++ KiwiBot/KiwiBot/Program.cs
@@ -24,46 +24,26 @@ namespace Hero_Simple_Application5
 
                 //TODO: Needs some type of BATTERY VOLTAGE CHECK, maybe vibe contoller when low?
 
+                //NOTE: Magic Math Stolen from https://makezine.com/projects/make-40/kiwi/
+                //and Thad's Wilson Code: https://github.com/MetalCowRobotics/MCR15B---Wilson/blob/master/src/org/team4213/wilson/KiwiDrive.java
+
+
                 //THIS CONTROLLER CHECK IS A FAILSAFE CHECK, DO NOT REMOVE!!!!
                 if (gamepad.isConnected())
                 {
                     //float lx = gamepad.getLX();
-                    float _x = gamepad.getRX();
-                    float _y = gamepad.getRY();
-                    //Debug.Print("RX:" + _x + "   RY:" + _y);
+                    float x = -gamepad.getRX();
+                    float y = gamepad.getRY();
+                    float omega = -gamepad.getLX();
 
+                     double w1 = -x + omega * 0.5;
+                     double w2 = 0.5 * x + 0.866 * y + omega * 0.5;
+                     double w3 = -(-0.5 * x + 0.866 * y - omega * 0.5);
 
-                    //Convert them to a direction vector
-                    double theta = (_y == 0 || _x == 0) ? 0 : System.Math.Atan(_x / _y); //ternary check for divide-by-zero
-                    double magnitude = System.Math.Sqrt((_x * _x) + (_y * _y));
-
-                    //Debug.Print("Theta:" + theta + "     magnitude:" + magnitude);
-
-                    if (magnitude > 0.15f) //I think this is to avoid inputStick error?
-                    { //NOTE: Magic Math Stolen from https://makezine.com/projects/make-40/kiwi/
-                        double vy = magnitude * System.Math.Cos(theta);
-                        if (_y < 0) { vy *= -1; }
-
-                        double vx = magnitude * System.Math.Sin(theta);
-                        double sqrt3o2 = 1.0 * System.Math.Sqrt(3 / 2);
-
-                        double backWheel = -vx;
-                        double frontRightWheel = 0.5 * vx + sqrt3o2 * vy;
-                        double frontLeftWheel = 0.5 * vx - sqrt3o2 * vy;
-
-                        //Im not sure what this speed is... vector intensity?  is it percent? Can't be... hmm...
-                        Debug.Print("  backWheel:" + backWheel + "  Left:" + frontLeftWheel + "  Right:" + frontRightWheel);
-                        motor1.Set(ControlMode.PercentOutput, frontLeftWheel);
-                        motor2.Set(ControlMode.PercentOutput, frontRightWheel);
-                        motor3.Set(ControlMode.PercentOutput, backWheel);
-
-                    }
-                    else
-                    { // stick is idle, stop the wheels
-                        motor1.Set(ControlMode.PercentOutput, 0);
-                        motor2.Set(ControlMode.PercentOutput, 0);
-                        motor3.Set(ControlMode.PercentOutput, 0);
-                    }
+                        Debug.Print("  w1:" + w1 + "  w2:" + w2 + "  w3:" + w3);
+                        motor1.Set(ControlMode.PercentOutput, w3);
+                        motor2.Set(ControlMode.PercentOutput, w2);
+                        motor3.Set(ControlMode.PercentOutput, w1);
 
 
                     CTRE.Phoenix.Watchdog.Feed(); //this keeps the bot active... just leave it alone
